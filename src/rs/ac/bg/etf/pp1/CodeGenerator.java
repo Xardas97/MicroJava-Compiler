a1f0e721e7ecd5be4afeaddf8efc364c1df7a8c8
Xardas97 <markomicovic19@yahoo.com>
2021-05-16 23:09:14 +0200

    Add Code Generator: Read, Assign, Inc, Dec, Method call

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -4,6 +4,7 @@ import rs.ac.bg.etf.pp1.ast.*;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
 
 public class CodeGenerator extends VisitorAdaptor {
     int mainPc = 0;
@@ -21,6 +22,19 @@ public class CodeGenerator extends VisitorAdaptor {
     }
 
     public void visit(MethodDecl methodDecl) {
+        Struct type = methodDecl.getMethodTypeName().obj.getType();
+        if (type == Tab.noType) {
+            Code.put(Code.exit);
+            Code.put(Code.return_);
+        }
+    }
+
+    public void visit(ReturnStmt returnStmt) {
+        Code.put(Code.exit);
+        Code.put(Code.return_);
+    }
+
+    public void visit(EmptyReturnStmt returnStmt) {
         Code.put(Code.exit);
         Code.put(Code.return_);
     }
@@ -51,6 +65,50 @@ public class CodeGenerator extends VisitorAdaptor {
         }
         else {
             Code.put(Code.print);
+        }
+    }
+
+    public void visit(ReadStmt readStmt) {
+        if(readStmt.getDesignator().obj.getType().compatibleWith(Tab.charType)) {
+            Code.put(Code.bread);
+        }
+        else {
+            Code.put(Code.read);
+        }
+
+        Code.store(readStmt.getDesignator().obj);
+    }
+
+    public void visit(AssignmentStmt assignment) {
+        Code.store(assignment.getDesignator().obj);
+    }
+
+    public void visit(DesignatorFctr designatorFctr) {
+        Code.load(designatorFctr.getDesignator().obj);
+    }
+
+    public void visit(IncrementStmt incrementStmt) {
+        Code.load(incrementStmt.getDesignator().obj);
+        Code.loadConst(1);
+        Code.put(Code.add);
+        Code.store(incrementStmt.getDesignator().obj);
+    }
+
+    public void visit(DecrementStmt decrementStmt) {
+        Code.load(decrementStmt.getDesignator().obj);
+        Code.loadConst(1);
+        Code.put(Code.sub);
+        Code.store(decrementStmt.getDesignator().obj);
+    }
+
+    public void visit(MethodCall methodCall) {
+        Obj methodObj = methodCall.getDesignator().obj;
+        int offset = methodObj.getAdr() - Code.pc;
+        Code.put(Code.call);
+        Code.put2(offset);
+
+        if (methodCall.getParent() instanceof MethodCallStmt && methodCall.struct != Tab.noType) {
+            Code.put(Code.pop);
         }
     }
 }
