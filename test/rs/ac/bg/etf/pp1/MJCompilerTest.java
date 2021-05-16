f5929733f93797595dbbbc7e3db46bc284286c60
Xardas97 <markomicovic19@yahoo.com>
2021-05-16 15:43:12 +0200

    Add Code Generator: Initial

--- test/rs/ac/bg/etf/pp1/MJCompilerTest.java
+++ test/rs/ac/bg/etf/pp1/MJCompilerTest.java
@@ -2,6 +2,7 @@ package rs.ac.bg.etf.pp1;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -12,6 +13,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 import java_cup.runtime.*;
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.Obj;
 import rs.etf.pp1.symboltable.concepts.Scope;
@@ -64,6 +66,17 @@ public class MJCompilerTest {
             }
 
             log.info("Parsiranje gotovo!");
+
+            File objFile = new File("test/program.obj");
+            if (objFile.exists()) objFile.delete();
+
+            CodeGenerator codeGenerator = new CodeGenerator();
+            program.traverseBottomUp(codeGenerator);
+            Code.dataSize = semAnalyzer.nVars;
+            Code.mainPc = codeGenerator.mainPc;
+            Code.write(new FileOutputStream(objFile));
+
+            log.info("Generisanje koda je zavrseno!");
         }
         catch (Exception e) {
             log.error(e.getMessage(), e);
